57bf74b484e9f574c4c5405aa033b33b4d15efe7
Sarthak <sgoel@apptio.com>
2024-01-20 19:27:35 +0530

    init code

---
+++ learn/src/main/java/com/tqcf/learn/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.tqcf.learn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+  
+  @GetMapping("/")
+  public String hello() {
+    return "hello";
+  }
+}
