654fafe387dcbd7e9cc0f312d5b99ff12ca2c7ed
Sarthak <sgoel@apptio.com>
2024-01-21 19:04:36 +0530

    Add index page

--- learn/src/main/java/com/tqcf/learn/controller/MainController.java
+++ learn/src/main/java/com/tqcf/learn/controller/MainController.java
@@ -2,13 +2,15 @@ package com.tqcf.learn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
 
   
   @GetMapping("/")
-  public String hello() {
-    return "hello";
+  public ModelAndView hello() {
+    ModelAndView modelAndView = new ModelAndView("index");
+    return modelAndView;
   }
 }
